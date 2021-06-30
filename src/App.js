2b1c79bc505b588fccae28803731e941a2b2b970
Kasper Bruus Frank <kbf@scadaminds.com>
2021-06-30 12:23:31 +0200

    use eui components to set up style of page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import {CountriesTable} from "./CountriesTable";
 import {useQuery} from "@apollo/client";
 import {COUNTRY_QUERY} from "./queries";
+import {EuiPage, EuiPageBody, EuiPageContent, EuiPageHeader} from "@elastic/eui";
 
 function App() {
-  const {loading, error, data} = useQuery(COUNTRY_QUERY);
-
-  console.log({loading, data, error});
+  const {loading, data} = useQuery(COUNTRY_QUERY);
 
   const countries = data?.countries ?? [];
 
   return (
     <main>
-      <header>
-        Countries List
-      </header>
-
-      <CountriesTable countries={countries} loading={loading}/>
+      <EuiPage restrictWidth={true}>
+        <EuiPageBody>
+          <EuiPageHeader pageTitle="Countries List" />
+          <EuiPageContent>
+            <CountriesTable countries={countries} loading={loading}/>
+          </EuiPageContent>
+        </EuiPageBody>
+      </EuiPage>
     </main>
   );
 }
