49cdefe1bb80312ee9361e66ca93185b0e366f24
Kasper Bruus Frank <kbf@scadaminds.com>
2021-06-30 14:22:22 +0200

    move consts out of component

--- src/components/CountriesTable.jsx
+++ src/components/CountriesTable.jsx
@@ -1,45 +1,43 @@
 import { EuiInMemoryTable } from '@elastic/eui';
 import React from 'react';
 
-export const CountriesTable = ({ countries, loading }) => {
-  const columns = [
-    {
-      field: 'name',
-      name: 'Country name',
-      sortable: true,
-    },
-    {
-      field: 'emoji',
-      name: 'Country flag',
-      truncateText: true,
-    },
-    {
-      field: 'continent',
-      name: 'Continent',
-      render: ({ name }) => name,
-      sortable: true,
-    },
-    {
-      field: 'code',
-      name: 'ISO code',
-      sortable: true,
-    },
-  ];
+export const CountriesTable = ({ countries, loading }) => (
+  <EuiInMemoryTable
+    items={countries}
+    columns={columns}
+    search={search}
+    loading={loading}
+    pagination={true}
+    sorting={true}
+  />
+);
 
-  const search = {
-    box: {
-      incremental: true,
-    },
-  };
+const columns = [
+  {
+    field: 'name',
+    name: 'Country name',
+    sortable: true,
+  },
+  {
+    field: 'emoji',
+    name: 'Country flag',
+    truncateText: true,
+  },
+  {
+    field: 'continent',
+    name: 'Continent',
+    render: ({ name }) => name,
+    sortable: true,
+  },
+  {
+    field: 'code',
+    name: 'ISO code',
+    sortable: true,
+  },
+];
 
-  return (
-    <EuiInMemoryTable
-      items={countries}
-      columns={columns}
-      search={search}
-      loading={loading}
-      pagination={true}
-      sorting={true}
-    />
-  );
+const search = {
+  box: {
+    incremental: true,
+  },
 };
